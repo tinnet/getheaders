96e6af02154394322884b319ea08955ae165ba73
Tinnet Coronam <tinnet@coronam.net>
2011-12-19 23:59:32 +0100

    print headers (html)

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send('Hello World!');
+    var html = "";
+    response.contentType('html');
+    for (var header in request.headers) {
+        response.write(header + ": " + request.headers[header]);
+        response.write("<br/>");
+    }
+    response.end();
 });
 
 var port = process.env.PORT || 3000;
