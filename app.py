86d6ff6850729d57f55c58858fbdd06fbb605b6e
Tinnet Coronam <tinnet@coronam.net>
2012-03-11 19:29:53 +0100

    add xml template

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import os
 
 from flask import Flask
+from flask import jsonify
 from flask import request
 from flask import render_template
 
 app = Flask(__name__)
 
+KNOWN_MIMES = ['text/html', 'application/xhtml+xml', 'application/json', 'application/xml']
+
 @app.route('/')
-def hello():
+def root():
     result = dict()
     for key, value in request.headers.iteritems():
         result[key] = value
@@ -15,11 +18,22 @@ def hello():
     result['remote_addr'] = request.remote_addr
     result['xhr'] = request.is_xhr
 
-    return render_template('default.html', headers=result)
+    return find_best_response_type(request, result)
 
+def find_best_response_type(req, headers):
+    best = req.accept_mimetypes.best_match(KNOWN_MIMES)
+
+    if best == 'text/html' or best == 'application/xhtml+xml':
+        return render_template('default.html', headers=headers)
+    
+    if best == 'application/json':
+        return jsonify(headers)
+        
+    if best == 'application/xml':
+        return render_template('default.xml', headers=headers)
+    return "UNKNOWN: " + best
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.debug = True
+    app.debug = bool(int(os.environ.get('DEBUG', 0)))
     app.run(host='0.0.0.0', port=port)
