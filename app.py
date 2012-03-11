0d56c9b21a78aa105b79df140aa9bee31e821330
Tinnet Coronam <tinnet@coronam.net>
2012-03-11 12:42:25 +0100

    added html template

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 import os
 
 from flask import Flask
+from flask import request
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    result = dict()
+    for key, value in request.headers.iteritems():
+        result[key] = value
+
+    result['remote_addr'] = request.remote_addr
+    result['xhr'] = request.is_xhr
+
+    return render_template('default.html', headers=result)
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
