6696e6faf7c841eb64209fd7a61c810517bdf51e
Tinnet Coronam <tinnet@coronam.net>
2012-03-11 19:51:57 +0100

    add file extension route (overrides content type)

--- app.py
+++ app.py
@@ -4,34 +4,60 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask import render_template
+from flask import make_response
 
 app = Flask(__name__)
 
 KNOWN_MIMES = ['text/html', 'application/xhtml+xml', 'application/json', 'application/xml']
 
-@app.route('/')
-def root():
+def getHeaders(req):
     result = dict()
-    for key, value in request.headers.iteritems():
+    for key, value in req.headers.iteritems():
         result[key] = value
 
-    result['remote_addr'] = request.remote_addr
-    result['xhr'] = request.is_xhr
+    result['remote_addr'] = req.remote_addr
+    result['xhr'] = req.is_xhr
+    
+    return result
 
-    return find_best_response_type(request, result)
+@app.route('/')
+def root():
+    return find_best_response_type(request, getHeaders(request))
+
+@app.route('/default.<extension>')
+def default(extension):
+    if extension == 'html':
+        resp = make_response(render_template('default.html', headers=getHeaders(request)))
+        resp.mimetype = 'text/html'
+        return resp
+
+    if extension == 'xml':
+       resp = make_response(render_template('default.xml', headers=getHeaders(request)))
+       resp.mimetype = 'application/xml'
+       return resp
+
+    if extension == 'json':
+        return jsonify(getHeaders(request))
+
+    return "UNKNOWN EXTENSION: " + extension
 
 def find_best_response_type(req, headers):
     best = req.accept_mimetypes.best_match(KNOWN_MIMES)
 
     if best == 'text/html' or best == 'application/xhtml+xml':
-        return render_template('default.html', headers=headers)
-    
+        resp = make_response(render_template('default.html', headers=headers))
+        resp.mimetype = best
+        return resp
+
+    if best == 'application/xml':
+       resp = make_response(render_template('default.xml', headers=headers))
+       resp.mimetype = best
+       return resp
+
     if best == 'application/json':
         return jsonify(headers)
-        
-    if best == 'application/xml':
-        return render_template('default.xml', headers=headers)
-    return "UNKNOWN: " + best
+
+    return "UNKNOWN CONTENTTYPE: " + best
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
